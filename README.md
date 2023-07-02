d822588810e54470a1b7371689954dc4f3e23bb5
Prashant Anantharaman <lists@prashant.at>
2023-07-02 18:07:37 -0400

    Proposed reorganization of the project; added a pyproject.toml file to track dependencies

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # FileTypeFetcher (FTF)
 Downloads desired filetypes from Common Crawl data
 
+# Contributing
 
+- Package installation uses [poetry](https://python-poetry.org/docs/basic-usage/), but this is subject to change in the future. 
 
+- We use [git-flow workflows](https://www.atlassian.com/git/tutorials/comparing-workflows/gitflow-workflow) for our development. Depending on the kind of feature you are contributing, create a `hotfix` branch or a `feature` branch. Installation instructions are [here](https://github.com/nvie/gitflow/wiki/Installation).
 
+- We also require a pre-commit hook. You can follow the instructions [here](https://pre-commit.com/#install) to install them.
+
+- You will need to install the hooks in the `yaml` file in the repository using the following command: `pre-commit install`.
+
+# License
+
+We've released this project under GPLv3. Check the LICENSE file for more details.
--- filetypefetcher/commands.py
+++ filetypefetcher/commands.py
@@ -1,81 +1,85 @@
-import multiprocessing as mp
 import argparse
+import multiprocessing as mp
 from os import path
 
-def init_parser():
 
+def init_parser():
     parser = argparse.ArgumentParser(
-    description = "Python package that downloads files from common crawler's database. Only a few extensions are currently supported"
+        description="Python package that downloads files from common\
+        crawler's database. Only a few extensions are currently supported."
     )
 
     parser.add_argument(
         "-l",
         "--limit",
-        help = "Number of images per filetype desired",
-        required = True,
-        metavar = "<limit>",
-        type = int
+        help="Number of images per filetype desired",
+        required=True,
+        metavar="<limit>",
+        type=int,
     )
 
     parser.add_argument(
         "-f",
         "--filetypes",
-        help = "Desired filetypes to fetch",
-        required = True,
-        nargs = "+",
-        type = str
+        help="Desired filetypes to fetch",
+        required=True,
+        nargs="+",
+        type=str,
     )
 
     parser.add_argument(
         "-p",
         "--num_procs",
-        help = "Number of processes to use, default is 1",
-        required = False,
-        default = 1,
-        type = int
+        help="Number of processes to use, default is 1",
+        required=False,
+        default=1,
+        type=int,
     )
 
     parser.add_argument(
         "-o",
         "--output",
-        help = "Output directory to store downloaded files",
-        required = True,
-        type = str
+        help="Output directory to store downloaded files",
+        required=True,
+        type=str,
     )
 
     return parser
 
+
 # returns 0 on success, returns 1 on fail
 def validate_args(args):
-    
     if args.limit <= 0:
         print("Limit (-l,--limit) must be a positive number")
         return 1
-    
+
     # if they want more processes than what's detected
     if args.num_procs > mp.cpu_count():
-        print("Too many processes, multiprocessing library detected only " + str(mp.cpu_count()) + " cores")
+        print(
+            "Too many processes, multiprocessing library detected only "
+            + str(mp.cpu_count())
+            + " cores"
+        )
         return 1
 
     if not path.isdir(args.output):
         print("Expected output (-o,--output) to be an existing directory")
         return 1
-    
+
     return 0
 
+
 def main():
-    
     # initialize parser and parse arguments using argparse library
     parser = init_parser()
     args = parser.parse_args()
-    
-    validate_args(args)
-    
-    limit = args.limit
-    filetypes = args.filetypes
-    num_procs = args.num_procs
 
-    
+    validate_args(args)
+
+    # limit = args.limit NOTE: This variable was never used.
+    filetypes = args.filetypes
+    # num_procs = args.num_procs NOTE: This variable was never used.
+
     # dict of # of files per filetype
     file_count = {}
     for ext in filetypes:
